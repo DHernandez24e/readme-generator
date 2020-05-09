49cd4970619bcf0571eb1cce2c6c3ea3bbf21dca
Doggo <dhernandez24e@hotmail.com>
2020-05-09 14:48:09 -0700

    adding questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,20 @@
-
+const inquirer = require('inquirer');
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github username? (Required)',
+        validate: githubName => {
+            if (githubName) {
+                return true;
+            } else {
+                console.log('Please enter your Github username.')
+                return false;
+            }
+        }
+    }
 
 ];
 
@@ -11,7 +24,7 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    return inquirer.prompt(questions)
 }
 
 // function call to initialize program
