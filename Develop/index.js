7f00c161d00c699d25dc4db72a188625fcbfdc38
Doggo <dhernandez24e@hotmail.com>
2020-05-10 01:49:23 -0700

    added questions to array

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // array of questions for user
 const questions = [
@@ -14,8 +15,68 @@ const questions = [
                 return false;
             }
         }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter an email address.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's name? (Required)",
+        validate: projectInput => {
+            if (projectInput) {
+                return true;
+            } else {
+                console.log('Please enter a name for your project.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of your project.'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None']
+    },
+    {
+        type: 'input',
+        name: 'depends',
+        message: 'What command should you run to install dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should you run to run tests?',
+        default: 'None'
+    },
+    {
+        type: 'input',
+        name: 'repoUse',
+        message: 'What does the user need to know about using the repo?',
+        default: 'Nothing'
+    },
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'What does the user need to know about contributing to the repo?',
+        default: 'Nothing'
     }
-
 ];
 
 // function to write README file
@@ -25,6 +86,7 @@ function writeToFile(fileName, data) {
 // function to initialize program
 function init() {
     return inquirer.prompt(questions)
+    .then(generateMarkdown)
 }
 
 // function call to initialize program
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,47 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data);
 
-`;
+//   return `# ${data.title}
+//   //License
+
+//   ## Description
+//   ${data.test}
+
+//   ## Table of Contents
+//   * [Installation] (#installation)
+//   * [Usage] (#usage)
+//   * [Credits] (#credits)
+//   * [License] (#license)
+//   * [Contributing] (#contributing)
+//   * [Tests] (#tests)
+//   * [Questions] (#questions)
+
+//   ## Installation
+    
+//     To install necessary dependencies, run the following command:
+    
+//     ${data.depends}
+  
+//   ## Usage
+
+//   ## License
+
+//     This project is licensed under the ${data.license} license
+
+//   ## Contributing
+
+//     ${data.contribute}
+
+//   ## Tests
+
+//     To run tests, run the following command
+
+//     ${data.tests}
+
+//   ## Questions
+//     If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at ${data.github}.  
+// `;
 }
 
 module.exports = generateMarkdown;
